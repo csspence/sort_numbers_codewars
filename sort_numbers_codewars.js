fbbeb95e80bd6a52a756908f0af9ba5ff3d814f3
Crew Spence <csspence123@gmail.com>
2020-08-15 06:39:44 -0500

    Wrote working solution using bubble sort.

--- sort_numbers_codewars.js
+++ sort_numbers_codewars.js
@@ -7,6 +7,20 @@ solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 solution(null); // should return []
 */
 
-function solution(nums){
+const solution = (arr) => {
+  let noSwaps;
+  for(let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for(let j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) {
+      break;
+    }
+  }
 
+  return arr;
 }
